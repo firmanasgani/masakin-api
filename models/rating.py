bfe0829d068cb25539ec58dfbe78b49090416899
Raden Wisnu Andhika Pranidhia <radenwisnu21@gmail.com>
2024-11-15 18:39:34 +0700

    feat: add RatingModel

---
+++ models/rating.py
@@ -0,0 +1,13 @@
+from db import db
+from sqlalchemy.orm import relationship
+
+class RatingModel(db.Model):
+    __tablename__ = "rating"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    users_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.id'), nullable=False)  
+    rating_value = db.Column(db.Intger, nullable=False)
+
+    recipe = relationship("RecipeModel", back_populates="rating")
+    user = relationship("UserModel", back_populates="rating")
