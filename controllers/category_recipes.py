71d1f8d9a343226aee9d5a3ed4727ca94fc3416e
finuaz <imans.office@gmail.com>
2024-11-15 17:46:37 +0700

    category controller based on recipe country

--- controllers/category_recipes.py
+++ controllers/category_recipes.py
@@ -1,26 +1,26 @@
-# import logging
+from flask.views import MethodView
+from flask_jwt_extended import (
+    get_jwt_identity,
+    jwt_required,
+)
+from flask_smorest import Blueprint, abort
+from sqlalchemy.exc import SQLAlchemyError
+from flask import jsonify, current_app
+from sqlalchemy import desc
 
-# from flask.views import MethodView
-# from flask_jwt_extended import (
-#     get_jwt_identity,
-#     jwt_required,
-# )
-# from flask_smorest import Blueprint, abort
-# from sqlalchemy.exc import SQLAlchemyError
-# from flask import jsonify, current_app
-# from sqlalchemy import desc
+from schemas.recipe import RecipeSchema
+from models.recipe import RecipeModel
 
-# from schemas.recipe import RecipeSchema
+recipe_schema = RecipeSchema()
+recipes_schema = RecipeSchema(many=True)
 
-
-# recipe_schema = RecipeSchema()
-# recipes_schema = RecipeSchema(many=True)
-
-# blp = Blueprint("categories", __name__, description="Operations on recipes")
+blp = Blueprint("categories", __name__, description="Operations on recipes")
 
 
-# @blp.route("/category/<string:category>")
-# class GetRecipeCategory(MethodView):
-#     @blp.response(200, recipes_schema)
-#     def get(self, category):
-#         return category
+@blp.route("/category/<string:category>")
+class GetRecipeCategory(MethodView):
+    @blp.response(200, recipes_schema)
+    def get(self, category):
+        recipes = RecipeModel.query.filter_by(country=category).all()
+        serialized_recipes = recipes_schema.dump(recipes)
+        return jsonify(serialized_recipes), 200
